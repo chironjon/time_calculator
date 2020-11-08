a371b1a446ace06513be5f33ce12123aa4c14959
ChironJon <immrwhiskers4@gmail.com>
2020-11-08 00:42:06 -0600

    with adding working how it does the equals button was pointless as it was. disabled for now along with subtract. Right now, adding works and that's what the original goal was

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 const allButtons = document.getElementsByClassName('numpad-button');
-
 // const allDisplays = document.getElementsByClassName('time-display');
 const hourInput = document.getElementById('hour-display');
 const minInput = document.getElementById('min-display');
 const secInput = document.getElementById('sec-display');
 const equationDisplay = document.getElementById('equation-display');
 const answerDisplay = document.getElementById('answer-display');
+
 // defaults
+let termsArr = ["0:0:0"];
+let buttonPressed = {'type': '', 'last': ''};
 hourInput.value = 0;
 minInput.value = 0;
 secInput.value = 0;
-equationDisplay.value = "equation here"
-answerDisplay.value = "answer here"
-// sample equation string --> downtoseconds(inputOne) + downtoseconds(inputTwo) = upfromseconds(seconds)
-// const displayedValues = {
-//   'hr': 0,
-//   'mn': 0,
-//   'sc': 0
-// };
-const termsArr = [];
-
-const buttonPressed = {
-  'type': '',
-  'last': ''
-}
+equationDisplay.value = termsArr[0];
+answerDisplay.value = termsArr[0];
 
 const timeToSeconds = (time) => {
   let hours = parseInt(time.split(':')[0]) * 60 * 60;
@@ -41,107 +31,83 @@ const secondsToTime = (seconds) => {
 }
 
 const equate = (pressedButton, buttonValue) => {
-  console.log(pressedButton, buttonValue, 'top of function')
   let pressedValue = parseInt(buttonValue);
-  // switch(buttonPressed.last){
-  //   case "op":
-  //     console.log('op pressed last');
-  //     break;
-  //   case "num":
-  //     console.log('num pressed last')
-  //     break;
-  //   default:
-  //     console.log('last button pressed error');
-  //     break;
-  // }
   switch (pressedButton) {
     
     case "add":
-      // buttonPressed.last = 'op';
-      termsArr.push(`${hourInput.value}:${minInput.value}:${secInput.value}`);
       termsArr.push(buttonValue);
-      console.log(termsArr[termsArr.length-1])
-      if(termsArr[termsArr.length-1].length < 2){
-        console.log('adding here')
-      }
+      termsArr.push(`${hourInput.value}:${minInput.value}:${secInput.value}`);
       equationDisplay.value = termsArr.join(' ');
+      answerDisplay.value = secondsToTime(timeToSeconds(termsArr[0]) + timeToSeconds(termsArr[2]));
+      for (let i = 0; i < termsArr.length; i++){}
+      termsArr = [answerDisplay.value];
       hourInput.value = 0;
       minInput.value = 0;
       secInput.value = 0;
-      console.log("add inside case", timeToSeconds(termsArr[0]));
+      console.log("add inside case");
       break;
     case "sub":
-      // buttonPressed.last = 'op';
-      termsArr.push(`${hourInput.value}:${minInput.value}:${secInput.value}`);
       termsArr.push(buttonValue);
+      termsArr.push(`${hourInput.value}:${minInput.value}:${secInput.value}`);
       equationDisplay.value = termsArr.join(' ');
+      answerDisplay.value = secondsToTime(timeToSeconds(termsArr[0]) - timeToSeconds(termsArr[2]));
+      termsArr = [answerDisplay.value];
       hourInput.value = 0;
       minInput.value = 0;
       secInput.value = 0;
       console.log('sub inside case');
       break;
     case "mul":
-      // buttonPressed.last = 'op';
       console.log('mul inside case');
       break;
     case "div":
-      // buttonPressed.last = 'op';
       console.log('div inside case');
       break;
     case "oneHr":
     case "fivHr":
     case "fifHr":
-      // buttonPressed.last = 'num';
       if (hourInput.value == 0) {
         hourInput.value = pressedValue;
         
-      }else {
+      } else {
         hourInput.value = parseInt(hourInput.value) + pressedValue;
-       }
-      console.log('hours', hourInput.value)
+      }
       break;
     case "oneMn":
     case "fivMn":
     case "fifMn":
-      // buttonPressed.last = 'num';
       if (minInput.value == 0) {
         minInput.value = pressedValue;
         
-      }else {
+      } else {
         minInput.value = parseInt(minInput.value) + pressedValue;
-       }
+      }
       if (minInput.value > 59){
         minInput.value = parseInt(minInput.value) - 60;
         equate('oneHr', 1);
       }
-      console.log('minutes', minInput.value)
       break;
     case "oneSc":
     case "fivSc":
     case "fifSc":
-      // buttonPressed.last = 'num';
       if (secInput.value == 0) {
         secInput.value = pressedValue;
         
-      }else {
+      } else {
         secInput.value = parseInt(secInput.value) + pressedValue;
-       }
+      }
       if (secInput.value > 59){
         secInput.value = parseInt(secInput.value) - 60;
         equate('oneMn', 1);
       }
-      console.log('seconds', secInput.value)
       break;
     case "nonHr":
-      // buttonPressed.last = 'num';
       hourInput.value = 0;
       break;
     case "nonMn":
-      // buttonPressed.last = 'num';
       minInput.value = 0;
       break;
     case "nonSc":
-      // buttonPressed.last = 'num';
       secInput.value = 0;
       break;
     case "equ":
@@ -150,9 +116,9 @@ const equate = (pressedButton, buttonValue) => {
       hourInput.value = 0;
       minInput.value = 0;
       secInput.value = 0;
-      equationDisplay.value = "equation here"
-      answerDisplay.value = "answer here"
-      timeArr = [];
+      termsArr = ["0:0:0"];
+      equationDisplay.value = termsArr[0];
+      answerDisplay.value = termsArr[0];
       buttonPressed = {
         'type': '',
         'last': ''
@@ -186,11 +152,3 @@ for(let i = 0; i < allButtons.length; i++){
 //   }
 //   return output.join('');
 // }
-
-
-// const one = () => {
-//   console.log('1');
-//   currentInput.shift();
-//   currentInput.push(1);
-//   timeInput.value = arrayToTimeString(currentInput);
-// };
